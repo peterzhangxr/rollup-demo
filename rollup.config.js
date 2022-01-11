b349228440d2c08c355ce77615432ffbfe70b45e
Peter Zhang <peter.zhang.henan@gmail.com>
2022-01-11 15:29:19 +0800

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ export default {
 	input: 'src/main.js',
 	output: {
 		file: 'dist/bundle.js',
-		format: 'cjs', // amd es6 iife cjs umd
+		format: 'umd', // amd es6 iife cjs umd
 		name: 'bundleName'
 	}
 }
